dc76bb9139ec987071976d9cbe2e78bd60acbfc1
Anuroop <anuroopjoy@gmail.com>
2021-11-13 00:21:18 +0530

    final

--- main.js
+++ main.js
@@ -1,57 +1,57 @@
 System.register([], function (_export) {
-    return {
-        execute: function () {
-            _export(
-                window.singleSpaAngularjs.default({
-                    angular: angular,
-                    mainAngularModule: 'main-module',
-                    uiRouter: true,
-                    preserveGlobal: false
-                })
-            );
-        }
-    };
+  return {
+    execute: function () {
+      _export(
+        window.singleSpaAngularjs.default({
+          angular: angular,
+          mainAngularModule: "main-module",
+          uiRouter: true,
+          preserveGlobal: false,
+        })
+      );
+    },
+  };
 });
 
-var app = angular.module('main-module', ['ui.router']);
+var app = angular.module("main-module", ["ui.router"]);
 
 app.config(function ($stateProvider) {
-    $stateProvider.state({
-        name: 'main',
-        url: '/home',
-        template: '<home />'
-    });
+  $stateProvider.state({
+    name: "main",
+    url: "/home",
+    template: "<home />",
+  });
 });
 
-app.component('home', {
-    template: `First Name: <input type="text" ng-model="firstName" ng-change="vm.log()"><br>
+app.component("home", {
+  template: `First Name: <input type="text" ng-model="firstName" ng-change="vm.log()"><br>
     Last Name: <input type="text" ng-model="lastName"><br>
     <br>
     Full Name: {{firstName + ' ' +  lastName }}`,
-    controllerAs: 'vm',
-    controller: function () {
-        var vm = this;
+  controllerAs: "vm",
+  controller: function () {
+    var vm = this;
 
-        vm.$onInit = function () {
-            console.log('mounting angularjs component');
-            console.log(
-                'legacyAngularjsApp status',
-                singleSpa.getAppStatus('legacyAngularjsApp')
-            );
-        };
+    vm.$onInit = function () {
+      console.log("mounting angularjs component");
+      console.log(
+        "legacyAngularApp status",
+        singleSpa.getAppStatus("legacyAngularApp")
+      );
+    };
 
-        vm.$onDestroy = function () {
-            console.log('unmounting angularjs component!');
-            console.log(
-                'legacyAngularjsApp status',
-                singleSpa.getAppStatus('legacyAngularjsApp')
-            );
-        };
-        vm.log = function () {
-            console.log(
-                'legacyAngularjsApp status',
-                singleSpa.getAppStatus('legacyAngularjsApp')
-            );
-        };
-    }
+    vm.$onDestroy = function () {
+      console.log(
+        "legacyAngularApp status",
+        singleSpa.getAppStatus("legacyAngularApp")
+      );
+      console.log("unmounting angularjs component!");
+    };
+    vm.log = function () {
+      console.log(
+        "legacyAngularApp status",
+        singleSpa.getAppStatus("legacyAngularApp")
+      );
+    };
+  },
 });
